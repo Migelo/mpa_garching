736f65878055e6b6e6b756d039d844b0595e9dee
Migelo <miha@filetki.si>
2017-06-24 12:25:38 +0200

    fix to cycle_r_max

--- 1/hist/cycle_r_max.py
+++ 1/hist/cycle_r_max.py
@@ -7,7 +7,7 @@ filename = __file__
 s, h, g = pg.prepare_zoom('/ptmp/mpa/naab/REFINED/M0977/SF_X/4x-2phase/out/snap_M0977_4x_470', gas_trace='/ptmp/mpa/naab/REFINED/M0977/SF_X/4x-2phase/gastrace_M0977_4x_disc-Uebler_070_470.dat')
 
 cycle_r_max = [item[item > 0] for item in s.gas['cycle_r_max'][s.gas['num_recycled'] > 0]]
-particle_mass = [average(item[item > 0]) for item in s.gas['mass_at_ejection'][s.gas['num_recycled'] > 0]]
+particle_mass = [np.average(item[item > 0]) for item in s.gas['mass_at_ejection'][s.gas['num_recycled'] > 0]]
 
 
 bins = np.logspace(-1, 6, 15)
@@ -40,20 +40,13 @@ ax.set_xlabel("$r_{max}$ [kpc]")
 ax.set_ylabel("mass [$10^{10}\ M_{\odot}$]")
 ax.set_xscale('log')
 ax.set_yscale('log')
-# ax.set_xlim((1, 2000))
-#ax.set_ylim((1e0, 10e6))
-#ax.hist(cycle_r_max, bins=bins)
-ax.yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
+ax.yaxis.set_major_formatter(plt.FormatStrFormatter('%.3f'))
 ax.grid(True)
 ax.step(x, y)
 
 ay.set_xlabel("$r_{max}$ [kpc]")
 ay.set_ylabel("count")
 ay.grid(True)
-# ay.set_xscale('log')
-# ay.set_yscale('log')
-# ay.set_xlim((1, 2000))
-# ay.set_ylim((1e-1, 10e6))
-ay.hist(cycle_r_max, bins=np.linspace(min(cycle_r_max)*.9, median(cycle_r_max)*4, 25))
+ay.hist(cycle_r_max, bins=np.linspace(min(cycle_r_max)*.9, np.median(cycle_r_max)*4, 25))
 
 plt.savefig(filename.split("/")[-1][:-3] + ".png", bbox_inches='tight')
