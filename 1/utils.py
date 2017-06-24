b8722774643b5a746239f951c14444e4ffd589fc
Migelo <miha@filetki.si>
2017-06-24 18:45:35 +0200

    add utils.py with usefull functions

---
+++ 1/utils.py
@@ -0,0 +1,24 @@
+def prepare_step(x, y):
+    '''
+    Prepare data for a nice step plot.
+
+    Args:
+        x (list):   List concaining bin edges.
+        y (list):   List containing bin values.
+
+    Returns:
+        x (list):   List containing corrected bin edges.
+        y (list):   List containing corrected bin values.
+    '''
+    if type(x) is not list: 
+        x = x.tolist()
+    if type(y) is not list:
+        y = y.tolist()
+
+    y.insert(0, y[0])
+    x.insert(0, x[0])
+    y.insert(0, 0)
+    x.append(x[-1])
+    y.append(0)
+    
+    return x, y
