5667382cea9f2010c116a47e46ca79063aaced0d
Migelo <miha@filetki.si>
2017-06-26 17:27:30 +0200

    set the statistic to mean from sum

--- 1/nrc_vs_r.py
+++ 1/nrc_vs_r.py
@@ -6,7 +6,7 @@ import utils
 
 filename = __file__
 
-type = ('disc-Uebler', 'disc', 'ball')[2]
+type = ('disc-Uebler', 'disc', 'ball')[0]
 s, h, g = pg.prepare_zoom('/ptmp/mpa/naab/REFINED/M0977/SF_X/4x-2phase/out/snap_M0977_4x_470', gas_trace='/ptmp/mpa/naab/REFINED/M0977/SF_X/4x-2phase/gastrace_M0977_4x_%s_070_470.dat' % (type))
 
 metals_ejection = [item[item > 0] for item in s.gas['metals_at_ejection'][s.gas['num_recycled'] > -1] / s.gas['mass_at_ejection'][s.gas['num_recycled'] > -1]]
@@ -20,7 +20,7 @@ for i, particle in enumerate(cycle_r_max):
         extended_nor.append(number_of_recycles[i])
 
 average_nor, edges_nor, count_nor = stats.binned_statistic(np.concatenate(cycle_r_max), extended_nor, statistic='mean', bins=np.linspace(0, 40, 41))
-average_z, edges, count = stats.binned_statistic(np.concatenate(cycle_r_max), np.concatenate(metals_ejection), statistic='sum', bins=np.linspace(0, 40, 41))
+average_z, edges, count = stats.binned_statistic(np.concatenate(cycle_r_max), np.concatenate(metals_ejection), statistic='mean', bins=np.linspace(0, 40, 41))
 
 edges, average_z = utils.prepare_step(edges, average_z)
 edges_nor, average_nor = utils.prepare_step(edges_nor, average_nor)
