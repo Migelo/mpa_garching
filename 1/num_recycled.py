3c4163901a759579964290db033be37dba017a4b
Migelo <miha@filetki.si>
2017-07-06 15:54:36 +0200

    fix incorrect usage of utils

--- 1/num_recycled.py
+++ 1/num_recycled.py
@@ -9,29 +9,25 @@ filename = __file__
 
 def plot(args):
     halo = args[0]
-    type = args[1]
+    definition = args[1]
 
     path = '/ptmp/mpa/naab/REFINED/%s/SF_X/4x-2phase/out/snap_%s_4x_???' % (halo, halo)
     max = int(sorted(glob.glob(path))[-1][-3:])
-    s, h, g = pg.prepare_zoom('/ptmp/mpa/naab/REFINED/%s/SF_X/4x-2phase/out/snap_%s_4x_%s' % (halo, halo, max), gas_trace='/u/mihac/data/%s/4x-2phase/gastrace_%s' % (halo, type), star_form=None)
+    s, h, g = pg.prepare_zoom('/ptmp/mpa/naab/REFINED/%s/SF_X/4x-2phase/out/snap_%s_4x_%s' % (halo, halo, max), gas_trace='/u/mihac/data/%s/4x-2phase/gastrace_%s' % (halo, definition), star_form=None)
 
-    x = range(max(s.gas['num_recycled']) + 1)
-    y = [s.gas['mass'][s.gas['num_recycled'] == i].sum() for i in range(max(s.gas['num_recycled']) + 1)]
+    x = range(s.gas['num_recycled'].max() + 1)
+    y = [s.gas['mass'][s.gas['num_recycled'] == i].sum() for i in range(s.gas['num_recycled'].max() + 1)]
     y = np.array(y)
-    fig, ax = plt.subplots(2)
+
+    fig, ax = plt.subplots(1)
+    plt.rcParams.update({'font.size': 16})
     plt.tight_layout()
 
-    ax[0].set_xlabel("$N_{recycled}$")
-    ax[0].set_ylabel("$M_{\odot}$")
-    ax[1].set_xlabel("$N_{recycled}$")
-    ax[1].set_ylabel("$M_{\odot}$")
-    ax[1].set_xlim((0, len(y[y > .1 * max(y)])))
-    ax[1].set_xticks(range(int(ax[1].get_xlim()[-1])))
+    ax.set_xlabel("$N_{recycled}$")
+    ax.set_ylabel("$M_{\odot}$")
+    ax.bar(x, y, width=1)
 
-    ax[0].bar(x, y, width=1)
-    ax[1].bar(x, y, width=1)
-
-    plt.savefig(filename.split("/")[-1][:-3] + '_' + halo + '_' + type + ".png", bbox_inches='tight')
+    plt.savefig(filename.split("/")[-1][:-3] + '_' + halo + '_' + definition + ".png", bbox_inches='tight')
 
 p = Pool(4)
 p.map(plot, utils.combinations)
--- 1/profiles.py
+++ 1/profiles.py
@@ -10,26 +10,27 @@ filename = __file__
 
 def plot(args):
     halo = args[0]
-    type = args[1]
+    definition = args[1]
 
     path = '/ptmp/mpa/naab/REFINED/%s/SF_X/4x-2phase/out/snap_%s_4x_???' % (halo, halo)
     max = int(sorted(glob.glob(path))[-1][-3:])
-    s, h, g = pg.prepare_zoom('/ptmp/mpa/naab/REFINED/%s/SF_X/4x-2phase/out/snap_%s_4x_%s' % (halo, halo, max), gas_trace='/u/mihac/data/%s/4x-2phase/gastrace_%s' % (halo, type), star_form=None)
+    s, h, g = pg.prepare_zoom('/ptmp/mpa/naab/REFINED/%s/SF_X/4x-2phase/out/snap_%s_4x_%s' % (halo, halo, max), gas_trace='/u/mihac/data/%s/4x-2phase/gastrace_%s' % (halo, definition), star_form=None)
 
-    r = np.array([np.sqrt(x**2 + y**2 + z**2) for x, y, z in s.gas['pos'][s.gas['num_recycled'] > -1]])
+    r = s.gas['r'][s.gas['num_recycled'] > -1] 
     nrc = s.gas['num_recycled'][s.gas['num_recycled'] > -1]
-    z = s.gas['metals'][s.gas['num_recycled'] > -1]/s.gas['mass'][s.gas['num_recycled'] > -1]
+    z = s.gas['metallicity'][s.gas['num_recycled'] > -1]
 
     nrc_avg, edges_nrc, count_nrc = stats.binned_statistic(r, nrc, statistic='mean', bins=np.linspace(0, 3000, 3000))
     z_avg, edges_z, count_z = stats.binned_statistic(r, z, statistic='mean', bins=np.linspace(0, 3000, 3000))
-    edges_nrc, nrc_avg = utils.prepare_step(edges_nrc, nrc_avg)
-    edges_z, z_avg = utils.prepare_step(edges_z, z_avg)
+    
+    nrc_avg = utils.prepare_step(nrc_avg)
+    z_avg = utils.prepare_step(z_avg)
 
     fig, (ax1, ax2) = plt.subplots(2)
     plt.tight_layout()
     ax1.grid(True)
     ax1.set_xlim((0, 100))
-    ax1.set_ylim((0, max(nrc[r < ax1.get_xlim()[1]])))
+    ax1.set_ylim((0, np.max(nrc[r < ax1.get_xlim()[1]])))
     ax1.set_xlabel('r [kpc]')
     ax1.set_ylabel('number of recycles')
     ax1.scatter(r, nrc)
@@ -44,7 +45,7 @@ def plot(args):
     ax2.scatter(r, z)
     ax2.step(edges_z, z_avg, c='r')
 
-    plt.savefig(filename.split("/")[-1][:-3] + '_' + halo + '_' + type + ".png", bbox_inches='tight')
+    plt.savefig(filename.split("/")[-1][:-3] + '_' + halo + '_' + definition + ".png", bbox_inches='tight')
 
 p = Pool(4)
 p.map(plot, utils.combinations)
