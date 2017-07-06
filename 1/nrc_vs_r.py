2dd18e0c85b36cf6ff9ca12b6fadb4c697f58414
Migelo <miha@filetki.si>
2017-07-06 15:38:59 +0200

    add plots

--- 1/nrc_vs_r.py
+++ 1/nrc_vs_r.py
@@ -10,11 +10,11 @@ filename = __file__
 
 def plot(args):
     halo = args[0]
-    type = args[1]
+    definition = args[1]
 
     path = '/ptmp/mpa/naab/REFINED/%s/SF_X/4x-2phase/out/snap_%s_4x_???' % (halo, halo)
     max = int(sorted(glob.glob(path))[-1][-3:])
-    s, h, g = pg.prepare_zoom('/ptmp/mpa/naab/REFINED/%s/SF_X/4x-2phase/out/snap_%s_4x_%s' % (halo, halo, max), gas_trace='/u/mihac/data/%s/4x-2phase/gastrace_%s' % (halo, type), star_form=None)
+    s, h, g = pg.prepare_zoom('/ptmp/mpa/naab/REFINED/%s/SF_X/4x-2phase/out/snap_%s_4x_%s' % (halo, halo, max), gas_trace='/u/mihac/data/%s/4x-2phase/gastrace_%s' % (halo, definition), star_form=None)
 
     metals_ejection = [item[item > 0] for item in s.gas['metals_at_ejection'][s.gas['num_recycled'] > -1] / s.gas['mass_at_ejection'][s.gas['num_recycled'] > -1]]
     metals_infall = [item[item > 0] for item in s.gas['metals_at_infall'][s.gas['num_recycled'] > -1] / s.gas['mass_at_infall'][s.gas['num_recycled'] > -1]]
@@ -29,8 +29,8 @@ def plot(args):
     average_nor, edges_nor, count_nor = stats.binned_statistic(np.concatenate(cycle_r_max), extended_nor, statistic='mean', bins=np.linspace(0, 40, 41))
     average_z, edges, count = stats.binned_statistic(np.concatenate(cycle_r_max), np.concatenate(metals_ejection), statistic='mean', bins=np.linspace(0, 40, 41))
 
-    edges, average_z = utils.prepare_step(edges, average_z)
-    edges_nor, average_nor = utils.prepare_step(edges_nor, average_nor)
+    average_z = utils.prepare_step(average_z)
+    average_nor = utils.prepare_step(average_nor)
 
     fig, ax = plt.subplots(1)
     ax.grid(True)
@@ -43,7 +43,7 @@ def plot(args):
     ax2.step(edges_nor, average_nor, color='r')
     ax2.set_ylabel('average number of recycles', color='r')
     ax2.tick_params('y', colors='r')
-    plt.savefig(filename.split("/")[-1][:-3] + '_' + halo + '_' + type + ".png", bbox_inches='tight')
+    plt.savefig(filename.split("/")[-1][:-3] + '_' + halo + '_' + definition + ".png", bbox_inches='tight')
 
 p = Pool(4)
 p.map(plot, utils.combinations)
