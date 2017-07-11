dd89c49313aa8d6648b119eac6e3657679b1d1cc
Migelo <miha@filetki.si>
2017-07-11 19:00:41 +0200

    new plots with different projection

--- 1/density_profiles.py
+++ 1/density_profiles.py
@@ -22,49 +22,43 @@ def plot(args):
     for axes in ax.flatten():
         axes.grid(True)
 
-    pg.plotting.profile(s.gas, '200 kpc', 'mass', ax=ax[0,0], label='total', proj=1)
-    pg.plotting.profile(s.gas[s.gas['num_recycled'] == -1], '200 kpc', 'mass', ax=ax[0,0], label='non-recycled', proj=1)
-    pg.plotting.profile(s.gas[np.max(s.gas['T_at_ejection'] > 0, axis=1)], '200 kpc', 'mass', ax=ax[0,0], label='recycled', proj=1)
+    pg.plotting.profile(s.gas, '200 kpc', 'mass', ax=ax[0,0], label='total')
+    pg.plotting.profile(s.gas[s.gas['num_recycled'] == -1], '200 kpc', 'mass', ax=ax[0,0], label='non-recycled')
+    pg.plotting.profile(s.gas[np.max(s.gas['T_at_ejection'] > 0, axis=1)], '200 kpc', 'mass', ax=ax[0,0], label='recycled')
     ax[0,0].set_ylim((1e4, 1e8))
     ax[0,0].legend(loc='upper right')
 
-    pg.plotting.profile(s.gas, '200 kpc', 'HI', ax=ax[0,1], label='total', proj=1)
-    pg.plotting.profile(s.gas[s.gas['num_recycled'] == -1], '200 kpc', 'HI', ax=ax[0,1], label='non-recycled', proj=1)
-    pg.plotting.profile(s.gas[np.max(s.gas['T_at_ejection'] > 0, axis=1)], '200 kpc', 'HI', ax=ax[0,1], label='recycled', proj=1)
+    pg.plotting.profile(s.gas, '200 kpc', 'HI', ax=ax[0,1], label='total')
+    pg.plotting.profile(s.gas[s.gas['num_recycled'] == -1], '200 kpc', 'HI', ax=ax[0,1], label='non-recycled')
+    pg.plotting.profile(s.gas[np.max(s.gas['T_at_ejection'] > 0, axis=1)], '200 kpc', 'HI', ax=ax[0,1], label='recycled')
     ax[0,1].set_ylim((1e0, 1e8))
-    # ax[0,1].legend(loc='upper right')
     
-    pg.plotting.profile(s.gas, '200 kpc', 'MgII', ax=ax[1,0], label='total', proj=1)
-    pg.plotting.profile(s.gas[s.gas['num_recycled'] == -1], '200 kpc', 'MgII', ax=ax[1,0], label='non-recycled', proj=1)
-    pg.plotting.profile(s.gas[np.max(s.gas['T_at_ejection'] > 0, axis=1)], '200 kpc', 'MgII', ax=ax[1,0], label='recycled', proj=1)
+    pg.plotting.profile(s.gas, '200 kpc', 'MgII', ax=ax[1,0], label='total')
+    pg.plotting.profile(s.gas[s.gas['num_recycled'] == -1], '200 kpc', 'MgII', ax=ax[1,0], label='non-recycled')
+    pg.plotting.profile(s.gas[np.max(s.gas['T_at_ejection'] > 0, axis=1)], '200 kpc', 'MgII', ax=ax[1,0], label='recycled')
     ax[1,0].set_ylim((1e-4, 1e5))
-    # ax[1,0].legend(loc='upper right')
 
-    pg.plotting.profile(s.gas, '200 kpc', 'CIV', ax=ax[1,1], label='total', proj=1)
-    pg.plotting.profile(s.gas[s.gas['num_recycled'] == -1], '200 kpc', 'CIV', ax=ax[1,1], label='non-recycled', proj=1)
-    pg.plotting.profile(s.gas[np.max(s.gas['T_at_ejection'] > 0, axis=1)], '200 kpc', 'CIV', ax=ax[1,1], label='recycled', proj=1)
+    pg.plotting.profile(s.gas, '200 kpc', 'CIV', ax=ax[1,1], label='total')
+    pg.plotting.profile(s.gas[s.gas['num_recycled'] == -1], '200 kpc', 'CIV', ax=ax[1,1], label='non-recycled')
+    pg.plotting.profile(s.gas[np.max(s.gas['T_at_ejection'] > 0, axis=1)], '200 kpc', 'CIV', ax=ax[1,1], label='recycled')
     ax[1,1].set_ylim((1e-1, 1e2))
-    # ax[1,1].legend(loc='upper right')
 
-    pg.plotting.profile(s.gas, '200 kpc', 'OVI', ax=ax[2,0], label='total', proj=1)
-    pg.plotting.profile(s.gas[s.gas['num_recycled'] == -1], '200 kpc', 'OVI', ax=ax[2,0], label='non-recycled', proj=1)
-    pg.plotting.profile(s.gas[np.max(s.gas['T_at_ejection'] > 0, axis=1)], '200 kpc', 'OVI', ax=ax[2,0], label='recycled', proj=1)
+    pg.plotting.profile(s.gas, '200 kpc', 'OVI', ax=ax[2,0], label='total')
+    pg.plotting.profile(s.gas[s.gas['num_recycled'] == -1], '200 kpc', 'OVI', ax=ax[2,0], label='non-recycled')
+    pg.plotting.profile(s.gas[np.max(s.gas['T_at_ejection'] > 0, axis=1)], '200 kpc', 'OVI', ax=ax[2,0], label='recycled')
     ax[2,0].set_ylim((1e0, 1e3))
-    # ax[2,0].legend(loc='upper right')
     
-    pg.plotting.profile(s.gas, '200 kpc', 'metallicity', ax=ax[2,1], label='total')
-    pg.plotting.profile(s.gas[s.gas['num_recycled'] == -1], '200 kpc', 'metallicity', ax=ax[2,1], label='non-recycled')
-    pg.plotting.profile(s.gas[np.max(s.gas['T_at_ejection'] > 0, axis=1)], '200 kpc', 'metallicity', ax=ax[2,1], label='recycled')
-    ax[2,1].set_ylim((1e-5, 1e-1))
-    # ax[2,1].legend(loc='upper right')
+    pg.plotting.profile(s.gas, '200 kpc', 'metallicity', ax=ax[2,1], label='total', dens=False)
+    pg.plotting.profile(s.gas[s.gas['num_recycled'] == -1], '200 kpc', 'metallicity', ax=ax[2,1], label='non-recycled', dens=False)
+    pg.plotting.profile(s.gas[np.max(s.gas['T_at_ejection'] > 0, axis=1)], '200 kpc', 'metallicity', ax=ax[2,1], label='recycled', dens=False)
+    ax[2,1].set_ylim((1e-1, 1e3))
 
     f.tight_layout()
     plt.subplots_adjust(top=0.93)
 
     
     plt.savefig(filename.split("/")[-1][:-3] + '_' + halo + '_' + definition + ".pdf", bbox_inches='tight')
-    print halo, definition
     
-p = Pool(7)
+p = Pool(8)
 p.map(plot, utils.combinations)
 
