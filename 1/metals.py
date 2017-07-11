f6699c123a65ce9c11f27c60cd44198b98871cdd
Migelo <miha@filetki.si>
2017-07-11 19:01:30 +0200

    implement pg.plotting.scatter instead of scatter

--- 1/metals.py
+++ 1/metals.py
@@ -1,4 +1,5 @@
 import pygad as pg
+import pygad.plotting
 import matplotlib.pyplot as plt
 import numpy as np
 from copy import copy
@@ -10,11 +11,11 @@ filename = __file__
 
 def plot(args):
     halo = args[0]
-    type = args[1]
+    definition = args[1]
 
     path = '/ptmp/mpa/naab/REFINED/%s/SF_X/4x-2phase/out/snap_%s_4x_???' % (halo, halo)
     max = int(sorted(glob.glob(path))[-1][-3:])
-    s, h, g = pg.prepare_zoom('/ptmp/mpa/naab/REFINED/%s/SF_X/4x-2phase/out/snap_%s_4x_%s' % (halo, halo, max), gas_trace='/u/mihac/data/%s/4x-2phase/gastrace_%s' % (halo, type), star_form=None)
+    s, h, g = pg.prepare_zoom('/ptmp/mpa/naab/REFINED/%s/SF_X/4x-2phase/out/snap_%s_4x_%s' % (halo, halo, max), gas_trace='/u/mihac/data/%s/4x-2phase/gastrace_%s' % (halo, definition), star_form=None)
 
     metals_ejection = [item[item > 0] for item in s.gas['metals_at_ejection'][s.gas['num_recycled'] > -1] / s.gas['mass_at_ejection'][s.gas['num_recycled'] > -1]]
     metals_infall = [item[item > 0] for item in s.gas['metals_at_infall'][s.gas['num_recycled'] > -1] / s.gas['mass_at_infall'][s.gas['num_recycled'] > -1]]
@@ -31,12 +32,11 @@ def plot(args):
     metals_infall_full = [item for sublist in metals_infall_full for item in sublist]
 
     fig, ax = plt.subplots(3)
-    plt.tight_layout()
     ax[0].set_xlabel("z at infall")
     ax[0].set_ylabel("z at ejection")
     ax[0].set_xlim((0, .1))
-    ax[0].set_ylim((0, .08))
-    ax[0].scatter(metals_infall, metals_ejection)
+    ax[0].set_ylim((0, .1))
+    pg.plotting.scatter_map(metals_infall, metals_ejection, ax=ax[0], logscale=True, extent=[[0, .1], [0, .1]])
     ax[0].plot([0, 1], [0, 1], color='r')
 
     ax[1].set_ylabel("count")
@@ -54,7 +54,8 @@ def plot(args):
     ax[2].hist(metals_ejection, bins=np.logspace(-4, -1, 31), alpha=.5, label='ejection')
     leg2 = plt.legend(loc='best')
 
-    plt.savefig(filename.split("/")[-1][:-3] + '_' + halo + '_' + type + ".png", bbox_inches='tight')
+    plt.tight_layout()
+    plt.savefig(filename.split("/")[-1][:-3] + '_' + halo + '_' + definition + ".png", bbox_inches='tight')
 
 p = Pool(4)
 p.map(plot, utils.combinations)
