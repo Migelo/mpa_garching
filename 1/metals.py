09fc07af94ffd0c8c48f665e5816419333044a38
Migelo <miha@filetki.si>
2017-07-13 00:52:50 +0200

    improve metals plot with 2d histogram

--- 1/metals.py
+++ 1/metals.py
@@ -6,6 +6,10 @@ from copy import copy
 import utils
 import glob
 from multiprocessing import Pool
+import matplotlib.gridspec as gridspec
+from scipy import stats
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+
 
 filename = __file__
 
@@ -31,30 +35,62 @@ def plot(args):
     metals_infall = [item for sublist in metals_infall for item in sublist]
     metals_infall_full = [item for sublist in metals_infall_full for item in sublist]
 
-    fig, ax = plt.subplots(3)
-    ax[0].set_xlabel("z at infall")
-    ax[0].set_ylabel("z at ejection")
-    ax[0].set_xlim((0, .1))
-    ax[0].set_ylim((0, .1))
-    pg.plotting.scatter_map(metals_infall, metals_ejection, ax=ax[0], logscale=True, extent=[[0, .1], [0, .1]])
-    ax[0].plot([0, 1], [0, 1], color='r')
+    count_infall, edges, foo = stats.binned_statistic(metals_infall,
+        metals_ejection, statistic='count', bins=np.logspace(-4, -1, 31))
+    count_ejection, edges, foo = stats.binned_statistic(metals_ejection,
+        metals_infall, statistic='count', bins=np.logspace(-4, -1, 31))
 
-    ax[1].set_ylabel("count")
-    ax[1].set_xlabel("z")
-    ax[1].set_xlim((0, .1))
-    ax[1].hist(metals_infall_full, bins=np.linspace(0, .1, 101), alpha=.5, label='infall')
-    ax[1].hist(metals_ejection, bins=np.linspace(0, .1, 101), alpha=.5, label='ejection')
-    leg1 = plt.legend(loc='best')
+#    normalisation = .1/float(np.max(count_infall))
+    count_infall = np.array(count_infall).astype(float) * 1e-1 / np.max(count_infall)
+    count_ejection = np.array(count_ejection).astype(float) * 1e-1 / np.max(count_ejection)
 
-    ax[2].set_ylabel("count")
-    ax[2].set_xlabel("z")
-    ax[2].set_xscale('log')
-    ax[2].set_yscale('log')
-    ax[2].hist(metals_infall_full, bins=np.logspace(-4, -1, 31), alpha=.5, label='infall')
-    ax[2].hist(metals_ejection, bins=np.logspace(-4, -1, 31), alpha=.5, label='ejection')
-    leg2 = plt.legend(loc='best')
 
-    plt.tight_layout()
+    f = plt.figure(figsize=(8.268, 8.268*2**.5))
+    gs = gridspec.GridSpec(4, 1)
+    ax1 = plt.subplot(gs[:2, 0])
+    ax2 = plt.subplot(gs[2, 0])
+    ax3 = plt.subplot(gs[3, 0])
+
+    bins, x_edge, y_edge = np.histogram2d(metals_ejection, metals_infall,
+        bins=[np.logspace(-4, 0, 61), np.logspace(-4, 0, 61)])
+    vmin, vmax = np.percentile(utils.finite(np.log10(bins.flatten())), [0, 95])
+    
+    abc = ax1.pcolormesh(x_edge, y_edge, np.log10(bins), vmin=vmin, vmax=vmax)
+    cax = inset_axes(ax1, width="70%", height="3%", loc=1)
+    cbar = f.colorbar(abc, cax=cax, orientation='horizontal')
+    cbar.set_label('$log_{10}(count)$', color='w')
+    cbar_obj = plt.getp(cbar.ax.axes, 'xticklabels')
+    plt.setp(cbar_obj, color='w')
+    
+    ax1.plot([0, 1], [0, 1], color='r')
+    ax1.set_xscale('log')
+    ax1.set_yscale('log')
+    ax1.set_xlim((1e-4, 1e0))
+    ax1.set_ylim((1e-4, 1e0))
+    ax1.set_xlabel("z at infall")
+    ax1.set_ylabel("z at ejection")
+    #ax1.bar(edges[:-1], count_infall, width=np.diff(edges), alpha=.3)
+    #ax1.barh(edges[:-1], count_ejection, height=np.diff(edges), alpha=.3)
+    #ax1.scatter(metals_infall, metals_ejection, alpha=.1, edgecolor=None)
+
+    ax2.set_ylabel("count")
+    ax2.set_xlabel("z")
+    ax2.set_xlim((0, .1))
+    ax2.hist(metals_infall, bins=np.linspace(0, .1, 101), alpha=.5, label='infall')
+    ax2.hist(metals_ejection, bins=np.linspace(0, .1, 101), alpha=.5, label='ejection')
+    leg1 = ax2.legend(loc='best')
+
+    ax3.set_ylabel("count")
+    ax3.set_xlabel("z")
+    ax3.set_xscale('log')
+    ax3.set_yscale('log')
+    ax3.hist(metals_infall, bins=np.logspace(-4, -1, 31), alpha=.5, label='infall')
+    ax3.hist(metals_ejection, bins=np.logspace(-4, -1, 31), alpha=.5, label='ejection')
+    leg2 = ax3.legend(loc='best')
+
+    f.tight_layout()
+    plt.subplots_adjust(top=0.93)
+    f.suptitle('%s - %s' % (halo, definition), fontsize=20)
     plt.savefig(filename.split("/")[-1][:-3] + '_' + halo + '_' + definition + ".png", bbox_inches='tight')
 
 p = Pool(4)
--- 1/utils.py
+++ 1/utils.py
@@ -17,17 +17,14 @@ def prepare_step(y):
     return y
 
 '''Generate combinations of halos with all tracing types.'''
-halos =  ('M0125', 'M0204', 'M0290', 'M0616', 'M0858', 'M0977', 'M1196', 'M1859', 'M4349',
-         'M0175', 'M0408', 'M0664', 'M0959', 'M1646', 'M4323', 'M6782')[::-1]
-#halos =  ('M6782')
-#halos =  ('M1196',)
+halos = ('M0408', 'M0501', 'M0616', 'M0664', 'M0858', 'M0959', 'M0977', 'M1192', 'M1196', 'M1646', 'M1859', 'M2283')
 #types =  ('disc-Uebler', 'disc', 'ball', 'ism')
 types =  ('ism', )
 combinations = []
 for halo in halos:
     for type in types:
         combinations.append((halo, type))
-# shuffle(combinations)
+shuffle(combinations)
 
 def save(path, filename):
     '''
@@ -42,3 +39,6 @@ def save(path, filename):
     if not os.path.isdir(path):
         os.mkdir(path)
     plt.savefig('%s/%s' % (path, filename))
+
+def finite(data):
+    return data[np.isfinite(data)]
